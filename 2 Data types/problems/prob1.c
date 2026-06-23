3b9e6b334f274844f699b78accb43b84e38501de
LakshmiGNair <gitalachu123@gmail.com>
2026-06-23 17:07:09 +0530

    new

---
+++ 2 Data types/problems/prob1.c
@@ -0,0 +1,12 @@
+// Create a program to input name of the person and respond with "Welcome NAME to KG Coding"
+
+#include<stdio.h>
+int main()
+{
+  char name;
+  printf("enter your name:");
+  scanf("%s",&name);
+  printf("Welcome %s to KG Coding");
+  return 0;
+
+}
